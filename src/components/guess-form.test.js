3b66f88391a73d6110a5f5ae6fef8b7582f61eb7
Camila Salvador <camilasalvador2@gmail.com>
2017-12-11 20:16:01 -0500

    created smoke tests for all components

--- src/components/guess-form.test.js
+++ src/components/guess-form.test.js
@@ -2,3 +2,9 @@ import React from 'react';
 import {shallow} from 'enzyme';
 
 import GuessForn from './guess-form';
+
+describe('<GuessForm />', () => {
+    it('Renders without crashing', () => {
+        shallow(<GuessForm />);
+    });
+});
--- src/components/guess-list.test.js
+++ src/components/guess-list.test.js
@@ -2,3 +2,9 @@ import React from 'react';
 import {shallow} from 'enzyme';
 
 import GuessList from './guess-list';
+
+describe('<GuessList />', () => {
+    it('Renders without crashing', () => {
+        shallow(<GuessList />);
+    });
+});
