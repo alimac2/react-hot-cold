83465303596accddc1709996a2057301c314fe16
Camila Salvador <camilasalvador2@gmail.com>
2017-12-30 15:05:47 -0500

    edit aural update reducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,4 +1,4 @@
-import { MAKE_GUESS } from '../actions';
+import { MAKE_GUESS, RESTART_GAME, GENERATE_AURAL_UPDATE } from '../actions';
 
 const initialState = {
     guesses: [1, 50, 30],
@@ -12,19 +12,36 @@ const initialState = {
 export default function gameReducer(state=initialState, action) {
     if (action.type === MAKE_GUESS ) {
         const guessArr = [...state.guesses, action.guess];
+        
         return Object.assign({}, state, { 
             guesses: guessArr
         });
     }
-//     else if (action.type === ) {
-//         return Object.assign({}, state, {
+
+    // if (action.type === RESTART_GAME) {
+    //     return Object.assign({}, state, {
+    //         guesses: [],
+    //         feedback: 'Make your guess!',
+    //         auralStatus: '',
+    //         correctAnswer: Math.round(Math.random() * 100) + 1
+    //     });
+    // }
+
+    if (action.type === GENERATE_AURAL_UPDATE) {
+        const pluralize = action.guesses.length !== 1;
+
+        let  auralStatus = `Here's the status of the game right now: [action.feedback] You've made [action.guesses.lengt] ${pluralize ? 'guesses' : 'guess'}.`;
+
+        if (action.guesses.length > 0) {
+            auralStatus += ` ${pluralize ? 'In order of most- to least-recent, they are' : 'It was'}: ${guesses.reverse().join(', ')}`;
+        }
+
+        this.setState({ auralStatus });
+        return Object.assign({}, state, {
             
-//         });
-//     }
-//     else if (action.type === ) {
-//         return Object.assign({}, state, {
-            
-//         });
-//     }
+        });
+    }
+
+
     return state;
 };
