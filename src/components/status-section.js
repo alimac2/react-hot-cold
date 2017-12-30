c4221a455d5d3b190db60e85b7eeedeb2b3cae03
Camila Salvador <camilasalvador2@gmail.com>
2017-12-29 22:19:05 -0500

    remove redundant guess count props

--- src/components/status-section.js
+++ src/components/status-section.js
@@ -6,11 +6,11 @@ import AuralStatus from './aural-status';
 
 export default function StatusSection(props) {
   const { guesses, auralStatus } = props;
-  const guessCount = guesses.length;
+
 
   return (
     <section aria-labelledby="guessCount" aria-describedby="guessList">
-      <GuessCount guessCount={guessCount} />
+      <GuessCount />
       <GuessList />
       <AuralStatus auralStatus={auralStatus} />
     </section>
