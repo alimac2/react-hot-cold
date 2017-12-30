b94e74300794ccfb334ca7ea52ec728212802fb7
Camila Salvador <camilasalvador2@gmail.com>
2017-12-30 18:00:12 -0500

    import connect to guess form file

--- src/components/guess-form.js
+++ src/components/guess-form.js
@@ -1,5 +1,5 @@
 import React from 'react';
-
+// import { connect } from 'react-redux';
 import './guess-form.css';
 
 export default class GuessForm extends React.Component {
@@ -41,3 +41,9 @@ export default class GuessForm extends React.Component {
     );
   }
 }
+
+// const mapStateToProps = state => ({
+//   guesses
+// });
+
+// export default connect()(GuessForm);
