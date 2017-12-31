94497eeb853d2ea0a05e28a4af6998e35fa5dfbe
Camila Salvador <camilasalvador2@gmail.com>
2017-12-30 19:41:00 -0500

    clean up

--- src/actions/index.js
+++ src/actions/index.js
@@ -3,21 +3,13 @@ export const makeGuess = (guess) => ({
     type: MAKE_GUESS,
     guess
 });
-/* should feeback be a property of makeGuess? Where is guess being used if the state has guesses as a property?
-
-Should all properties of the state be included in actions? what about correctAnswer?
-*/
-
 
 export const RESTART_GAME = "RESTART_GAME";
 export const restartGame = () => ({
     type: RESTART_GAME
 });
-/*I'm not sure if restart game should have another property. */
-
 
 export const GENERATE_AURAL_UPDATE  = "GENERATE_AURAL_UPDATE";
 export const generateAuralUpdate = () => ({
     type: GENERATE_AURAL_UPDATE
 });
-/* should auralStatus be here? I don't understand why or why not. */
